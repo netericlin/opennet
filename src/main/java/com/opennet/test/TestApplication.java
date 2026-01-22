15df2b7736f181e83b5602cbac2b6281158ffebe
Eric Lin <neteric1122@msn.com>
2026-01-22 21:58:05 +0800

    Add HelloControl testing

--- src/main/java/com/opennet/test/TestApplication.java
+++ src/main/java/com/opennet/test/TestApplication.java
@@ -1,10 +1,24 @@
 package com.opennet.test;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class TestApplication {
+
+	 @Value("${spring.application.name:-}")
+    String name;
+
+    @RestController
+    class HelloController {
+        @GetMapping("/")
+        String hello() {
+            return "Hello " + name + "!";
+        }
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
--- src/test/java/com/opennet/test/TestApplicationTests.java
+++ src/test/java/com/opennet/test/TestApplicationTests.java
@@ -1,9 +1,7 @@
 package com.opennet.test;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class TestApplicationTests {
 
 	@Test
